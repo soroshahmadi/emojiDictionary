5972f2c8cbe81b55422a1010905893f935fd37ca
soroshahmadi <soroshahmadi@outlook.com>
2017-02-13 21:01:13 -0800

    Added navigation controller

--- emojiDictionary/ViewController.swift
+++ emojiDictionary/ViewController.swift
@@ -36,6 +36,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return cell //it will return the cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "moveSegue", sender: "⚽️")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
